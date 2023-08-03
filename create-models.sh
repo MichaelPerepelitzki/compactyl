bec41c367ff6e8879129ad442e3a04f38a755cb8
michael <212941470@idf.il>
2023-08-03 21:29:00 +0300

    add linux supprt + remove logging to external file

--- create-models.sh
+++ create-models.sh
@@ -1,32 +1,36 @@
 # based on similar changes from alexoterof in okke-formsma/dactyl-manuform-tight
 
 # MAC
-OPEN_SCAD='/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD'
+# OPEN_SCAD='/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD'
 
 # WINDOWS
 # OPEN_SCAD='openscad.com'
+
+# LINUX
+OPEN_SCAD='openscad'
 
 INPUTDIR='things'
 OUTPUTDIR='things/compactyl-v6.0-5x6-hotswap'
 
 echo 'Generating things/*.scad files using '$(lein -v)' to '$OUTPUTDIR
-echo '(load-file "src/dactyl_keyboard/dactyl.clj")' | lein repl > /dev/null 2>&1
+# echo '(load-file "src/dactyl_keyboard/dactyl.clj")' | lein repl > /dev/null 2>&1
 
 echo 'Rendering '$OUTPUTDIR'/*.stl files from '$INPUTDIR'/*.scad using ...'
 $OPEN_SCAD -v
 # $OPEN_SCAD -o $OUTPUTDIR/usb-holder_vertical.stl     $INPUTDIR/usb_holder.scad >/dev/null 2>&1 &
 # $OPEN_SCAD -o $OUTPUTDIR/single-plate.stl           $INPUTDIR/single-plate.scad >/dev/null 2>&1 &
-$OPEN_SCAD -o $OUTPUTDIR/wrist-rest-right-holes.stl $INPUTDIR/wrist-rest-right-holes.scad >/dev/null 2>&1 &
+$OPEN_SCAD -o $OUTPUTDIR/wrist-rest-right-holes.stl $INPUTDIR/wrist-rest-right-holes.scad &
+$OPEN_SCAD -o $OUTPUTDIR/wrist-rest-left-holes.stl $INPUTDIR/wrist-rest-left-holes.scad &
 # $OPEN_SCAD -o $OUTPUTDIR/right-plate.stl            $INPUTDIR/right-plate.scad >/dev/null 2>&1 &
 # $OPEN_SCAD -o $OUTPUTDIR/right.stl                  $INPUTDIR/right.scad >/dev/null 2>&1 &
 # $OPEN_SCAD -o $OUTPUTDIR/left.stl                   $INPUTDIR/left.scad >/dev/null 2>&1 &
 
-$OPEN_SCAD -o $OUTPUTDIR/case-walls-right.stl    $INPUTDIR/case-walls-right.scad >/dev/null 2>&1 &
-$OPEN_SCAD -o $OUTPUTDIR/case-walls-left.stl     $INPUTDIR/case-walls-left.scad >/dev/null 2>&1 &
-$OPEN_SCAD -o $OUTPUTDIR/switch-plates-right.stl $INPUTDIR/switch-plates-right.scad >/dev/null 2>&1 &
-$OPEN_SCAD -o $OUTPUTDIR/switch-plates-left.stl  $INPUTDIR/switch-plates-left.scad >/dev/null 2>&1 &
-$OPEN_SCAD -o $OUTPUTDIR/bottom-plate-right.stl  $INPUTDIR/bottom-plate-right.scad >/dev/null 2>&1 &
-$OPEN_SCAD -o $OUTPUTDIR/bottom-plate-left.stl   $INPUTDIR/bottom-plate-left.scad >/dev/null 2>&1 &
+$OPEN_SCAD -o $OUTPUTDIR/case-walls-right.stl    $INPUTDIR/case-walls-right.scad &
+$OPEN_SCAD -o $OUTPUTDIR/case-walls-left.stl     $INPUTDIR/case-walls-left.scad &
+$OPEN_SCAD -o $OUTPUTDIR/switch-plates-right.stl $INPUTDIR/switch-plates-right.scad &
+$OPEN_SCAD -o $OUTPUTDIR/switch-plates-left.stl  $INPUTDIR/switch-plates-left.scad &
+$OPEN_SCAD -o $OUTPUTDIR/bottom-plate-right.stl  $INPUTDIR/bottom-plate-right.scad &
+$OPEN_SCAD -o $OUTPUTDIR/bottom-plate-left.stl   $INPUTDIR/bottom-plate-left.scad &
 
 echo 'Please wait for STL files to appear in '$OUTPUTDIR'/ directory!'
 
